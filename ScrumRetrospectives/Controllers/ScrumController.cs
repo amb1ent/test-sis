3e175ec1f1527150b1c2ce22807de022bc6b3d7f
Ben Gould <ben+dev@amb1ent.org>
2016-11-13 08:07:22 +0000

    web services, chose wrong a2 json shim, try another

--- ScrumRetrospectives/Controllers/ScrumController.cs
+++ ScrumRetrospectives/Controllers/ScrumController.cs
@@ -4,36 +4,40 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ScrumRetrospectives.Models;
 
 namespace ScrumRetrospectives.Controllers
 {
     public class ScrumController : ApiController
     {
         // GET: api/Scrum
-        public IEnumerable<string> Get()
+        public IEnumerable<ScrumApiModels.Retrospective> Get()
         {
-            return new string[] { "value1", "value2" };
+            return ScrumApiModels.DB.GetRetrospectives();
         }
 
         // GET: api/Scrum/5
-        public string Get(int id)
+        public ScrumApiModels.Retrospective Get(string id)
         {
-            return "value";
+            return ScrumApiModels.DB.GetRetrospective(id);
         }
 
         // POST: api/Scrum
-        public void Post([FromBody]string value)
+        public void Post([FromBody]ScrumApiModels.Retrospective value)
         {
+            throw new NotImplementedException();
         }
 
         // PUT: api/Scrum/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]ScrumApiModels.Retrospective value)
         {
+            throw new NotImplementedException();
         }
 
         // DELETE: api/Scrum/5
-        public void Delete(int id)
+        public void Delete(string id)
         {
+            ScrumApiModels.DB.DeleteRetrospective(id);
         }
     }
 }
--- ScrumRetrospectives/Models/ScrumApiModels.cs
+++ ScrumRetrospectives/Models/ScrumApiModels.cs
@@ -5,7 +5,50 @@ using System.Web;
 
 namespace ScrumRetrospectives.Models
 {
-    public class ScrumApiModels
+    public static class ScrumApiModels
     {
+        public static class DB
+        {
+            private static List<Retrospective> list = new List<Retrospective>();
+            private static Dictionary<string, Retrospective> dict = new Dictionary<string, Retrospective>();
+
+            static DB()
+            {
+                foreach (Retrospective r in new Retrospective[]
+                {
+                    new Retrospective
+                    {
+                        name = "Retrospective 1",
+                        summary = "Post release retrospective",
+                        date = "27/07/2016",
+                    }
+                })
+                {
+                    list.Add(r);
+                    dict.Add(r.name, r);
+                }
+            }
+            public static IEnumerable<Retrospective> GetRetrospectives()
+            {
+                return list;
+            } 
+            public static Retrospective GetRetrospective(string id)
+            {
+                return dict[id];
+            }
+
+            public static void DeleteRetrospective(string id)
+            {
+                list.Remove(dict[id]);
+                dict.Remove(id);
+            }
+        }
+
+        public class Retrospective
+        {
+            public string name { get; set; }
+            public string summary { get; set; }
+            public string date { get; set; }
+        }
     }
 }
